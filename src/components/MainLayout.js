74362c1e3defd06f7374f643cbe96306e64ffcc5
Chinh <tranxuanchinhxt2001@gmail.com>
2022-10-26 03:13:11 +0700

    Update MainLayout.js

--- src/components/MainLayout.js
+++ src/components/MainLayout.js
@@ -1,6 +1,48 @@
 import { Outlet } from "react-router-dom";
 import Footer from "./footer/Footer";
 import Navbar from "./header/Navbar";
+// icon
+import { ChevronUpIcon } from "@chakra-ui/icons";
+import { IconButton } from "@chakra-ui/react";
+import { useState } from "react";
+
+const ScrollToTopButton = () => {
+  const [visible, setVisible] = useState(false);
+
+  const transferVisile = () => {
+    const scrolled = document.documentElement.scrollTop;
+    if (scrolled > 300) {
+      setVisible(true);
+    } else {
+      setVisible(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  window.addEventListener("scroll", transferVisile);
+  return (
+    <IconButton
+      onClick={scrollToTop}
+      sx={{
+        display: visible ? "block" : "none",
+        position: "fixed",
+        right: "2%",
+        bottom: "40px",
+        background: "#FFFFFF",
+        boxShadow: " 0px 4px 4px rgba(0, 0, 0, 0.25)",
+        borderRadius: "25px",
+        border: "none",
+      }}
+      icon={<ChevronUpIcon color="black" boxSize={8} />}
+    />
+  );
+};
 
 const MainLayout = () => {
   return (
@@ -8,10 +50,9 @@ const MainLayout = () => {
       <Navbar />
       <Outlet />
       <Footer />
+      <ScrollToTopButton />
     </>
   );
 };
-
-<Outlet />;
 
 export default MainLayout;
